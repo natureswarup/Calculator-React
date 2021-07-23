06a596162aafc2e66b5771f3dcab2755d0f45320
Nature <nature.swarup@gmail.com>
2021-07-22 22:17:21 -0400

    got functionality working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [currentValue, setCurrentValue] = useState(0);
+  const [operator, setOperator] = useState("");
+  const [calcFinished, setCalcFinished] = useState(false);
+
+  const handleClick = (e) => {
+    setValue((prev) => prev + e.target.value);
+  };
+
+  const operate = (e) => {
+    setOperator(e.target.value);
+    if (calcFinished) {
+      setValue("");
+      setCalcFinished(false);
+      return;
+    }
+    setCurrentValue((prev) => prev + parseFloat(value));
+    setValue("");
+  };
+
+  const equals = (e) => {
+    if (operator === "+") {
+      setValue(parseFloat(value) + currentValue);
+      setCurrentValue(parseFloat(value) + currentValue);
+    }
+
+    if (operator === "-") {
+      setValue(currentValue - parseFloat(value));
+      setCurrentValue(currentValue - parseFloat(value));
+    }
+    if (operator === "*") {
+      setValue(currentValue * parseFloat(value));
+      setCurrentValue(currentValue * parseFloat(value));
+    }
+    if (operator === "/") {
+      setValue(currentValue / parseFloat(value));
+      setCurrentValue(currentValue / parseFloat(value));
+    }
+    setCalcFinished(true);
+  };
+
+  const clear = () => {
+    setValue("");
+    setOperator("");
+    setCurrentValue(0);
+    setCalcFinished(false);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calcContainer">
+        <div className="inputContainer">
+          <input type="number" name="answers" id="answers" value={value} />
+        </div>
+        <table>
+          <tr>
+            <td>
+              <input type="button" onClick={(e) => handleClick(e)} value="1" />
+            </td>
+            <td>
+              <input type="button" onClick={(e) => handleClick(e)} value="2" />
+            </td>
+
+            <td>
+              <input type="button" onClick={(e) => handleClick(e)} value="3" />
+            </td>
+            <td>
+              <input type="button" onClick={(e) => operate(e)} value="*" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="button" onClick={(e) => handleClick(e)} value="4" />
+            </td>
+            <td>
+              <input type="button" onClick={(e) => handleClick(e)} value="5" />
+            </td>
+            <td>
+              <input type="button" onClick={(e) => handleClick(e)} value="6" />
+            </td>
+            <td>
+              <input type="button" onClick={(e) => operate(e)} value="/" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="button" onClick={(e) => handleClick(e)} value="7" />
+            </td>
+            <td>
+              <input type="button" onClick={(e) => handleClick(e)} value="8" />
+            </td>
+            <td>
+              <input type="button" onClick={(e) => handleClick(e)} value="9" />
+            </td>
+            <td>
+              <input type="button" onClick={(e) => operate(e)} value="-" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="button" onClick={(e) => handleClick(e)} value="0" />
+            </td>
+            <td>
+              <input type="button" onClick={() => clear()} value="clr" />
+            </td>
+            <td>
+              <input type="button" onClick={(e) => equals(e)} value="=" />
+            </td>
+            <td>
+              <input type="button" onClick={(e) => operate(e)} value="+" />
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
